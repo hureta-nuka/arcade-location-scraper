56d6a982a42544bc2629c908929b2c1366ee3457
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2025-12-30 17:13:07 +0900

    Refactor scrape_shop_data function for improved readability and error handling; add monthly GitHub Actions workflow for automated data scraping; update dependencies in pyproject.toml and uv.lock to include ruff for code linting.

--- tests/test_save_to_json.py
+++ tests/test_save_to_json.py
@@ -3,29 +3,32 @@ from pathlib import Path
 from main import save_to_json
 import pytest
 
+
 @pytest.fixture
 def test_data():
-    '''テストデータ'''
-    return  {
-        "shops": [{
-            "name": "アミパラ佐世保店",
-            "operation_time": "10:00～24:00",
-            "holiday": "無し",
-            "latitude": "33.15005741",
-            "longitude": "129.7804145",
-            "tel": "0956-34-8880",
-            "address": "佐世保市大塔町18-15",
-            "access": "JR大塔駅徒歩3分"
+    """テストデータ"""
+    return {
+        "shops": [
+            {
+                "name": "アミパラ佐世保店",
+                "operation_time": "10:00～24:00",
+                "holiday": "無し",
+                "latitude": "33.15005741",
+                "longitude": "129.7804145",
+                "tel": "0956-34-8880",
+                "address": "佐世保市大塔町18-15",
+                "access": "JR大塔駅徒歩3分",
             }
         ],
         "count": 378,
         "source": "https://p.eagate.573.jp/game/facility/search/p/list.html",
         "timestamp": "2025-10-25T22:04:58.788059",
-        "description": "KONAMI e-amusement店舗検索から取得した全店舗データ"
+        "description": "KONAMI e-amusement店舗検索から取得した全店舗データ",
     }
 
+
 def test_save_to_json(tmp_path: Path, test_data: dict):
-    '''save_to_jsonが正しく動くかテスト'''
+    """save_to_jsonが正しく動くかテスト"""
     # 関数が存在するか確認
     assert callable(save_to_json)
 
@@ -43,4 +46,3 @@ def test_save_to_json(tmp_path: Path, test_data: dict):
         loaded_data = json.load(f)
 
     assert loaded_data == test_data, "ファイル内容が一致していません。"
-
